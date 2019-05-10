e4f68d190696f75f242d83a3bf932ed3b98a08fb
Rafael Borges <rafael.borges@justdigital.com.br>
2019-05-10 12:41:09 -0300

    Add netlify config

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(path.join(__dirname, 'src', '/index.html')),
+      template: path.resolve(path.join(__dirname, '/index.html')),
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
