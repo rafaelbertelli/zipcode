0130ef6b74885707975324e4f4645e35f5627928
Rafael Borges <rafael.borges@justdigital.com.br>
2019-05-10 12:32:22 -0300

    Fix historyApiFallback

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
+/* eslint-disable no-console */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './style/main.less';
 
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
 class App extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <Router>
+        {console.log('index')}
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </div>
+      </Router>
+    );
   }
 }
 
