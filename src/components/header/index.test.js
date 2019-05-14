5e1ed83018ab6eb8b2638f673013dd3517b0359a
Rafael Bertelli <rafaelbertelli89@gmail.com>
2019-05-14 09:42:29 -0300

    Tests Header component

--- src/components/header/index.test.js
+++ src/components/header/index.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header, { Form } from './index.js';
+import toJson from 'enzyme-to-json'
+import Header from './index.js';
+import { Form, Button } from './styled';
 
 describe('Header Test Suite', () => {
-  it('Should have a form', () => {
+  it('Should match snapshot', () => {
     const wrapper = shallow(<Header />);
-    const form = wrapper.find(Form);
-    expect(form).toBe(true);
+    expect(toJson(wrapper)).toMatchSnapshot()
   });
+
+  it('Should have one form', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find(Form)).toHaveLength(1)
+  })
+
+  it('Should have one button', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find(Button)).toHaveLength(1)
+  })
 });
